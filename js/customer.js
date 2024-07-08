50118e035df03352bf3c38db041f93ff4735f4a4
sunny9570 <sundy957063@gmail.com>
2024-07-08 17:29:35 +0900

    Add files via upload

---
+++ js/customer.js
@@ -0,0 +1,28 @@
+$(function () {
+
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+        if (sct > 0) {
+            $('#header').addClass('on')
+        } else {
+            $('#header').removeClass('on')
+        }
+    });
+
+
+    const mainVisualSlide = new Swiper('.main_visual_slide', {
+        // parallax: true,
+        loop: true,
+        speed: 1000,
+        autoplay: {
+            delay: 2500,
+            // disableOnInteraction: false,
+        },
+
+        navigation: {
+            nextEl: '#mainVisual .arrows .next',
+            prevEl: '#mainVisual .arrows .prev',
+        },
+    });
+
+})
