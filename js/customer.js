01e6c918f240cf1f8b227fe1db4363606c3cbac0
sunny9570 <sundy957063@gmail.com>
2024-07-09 17:28:25 +0900

    Add files via upload

--- js/customer.js
+++ js/customer.js
@@ -1,4 +1,42 @@
 $(function () {
+    $('#header .mbtn').on('click', function () {
+        $(this).toggleClass('on');
+        $('#header .gnb').toggleClass('on');
+
+    });
+
+    $('#header .snb>ul>li>a').on('click', function (e) {
+        if ($('#header .snb>ul>li ul').hasClass('on')) {
+            e.preventDefault();
+            $(this).next().stop().slideDown();
+            $(this).parent().siblings().find('ul').slideUp();
+
+        }
+
+    });
+    $(window).on('resize', function () {
+        $('#header .snb').removeClass('on');
+        $('#header .mbtn').removeClass('on');
+        $('#header .snb>ul>li ul').removeAttr('style')
+    });
+
+
+
+    $('.search .btn').on('click', function () {
+        $(this).next().fadeIn();
+
+    });
+    $('.search .close').on('click', function () {
+        $('#search').fadeOut();
+    });
+});
+
+
+
+
+
+
+$(function () {
 
     $(window).on('scroll', function () {
         let sct = $(window).scrollTop();
